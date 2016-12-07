a3a141a2b4f722bd47175298c69b34df97b99cdc
Frisch <wfrisch@kpmg.com>
2016-12-07 18:39:57 +0100

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var server = express();
+var favicon = require('serve-favicon');
 
 server.use(express.static(__dirname + '/public'));
  
+server.use(favicon(__dirname + '/public/favicon.ico'));
+
+
+
 var port = 3000;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
